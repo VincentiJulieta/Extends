116a0bb8e97fb5d7f2383e690d6c25dc9b5879ba
Vincenti Julieta <vincentijuli@gmail.com>
2022-06-02 21:34:10 -0300

    Initial commit

---
+++ src/A.java
@@ -0,0 +1,8 @@
+public class A {
+    public A() {
+        System.out.println("En A()");
+    }
+    public A (int i){
+        System.out.println("En A(i)");
+    }
+}
---
+++ src/B.java
@@ -0,0 +1,9 @@
+public class B extends A{
+     public B() {
+        System.out.println("En B()");
+    }
+    public B (int i){
+         super(i);
+        System.out.println("En B(i)");
+    }
+}
